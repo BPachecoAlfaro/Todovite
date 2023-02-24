454be94091d98e9e7043f4e04c879d7801a45acb
Bastian Pacheco <bpachecoalfaro@gmail.com>
2023-02-24 08:02:20 -0300

    TODO final Commit

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -1,7 +1,7 @@
 import { Todo } from "../todos/models/todo.model";
 
 
-const Filters = {
+export const Filters = {
     All: 'all',
     Completed: 'completed',
     Pending: 'Pending',
@@ -12,17 +12,27 @@ const state = {
         new Todo('Piedra del alma'),
         new Todo('Piedra del infinito'),
         new Todo('Piedra del tiempo'),
+        new Todo('Piedra del realidad'),
+        new Todo('Piedra del poder'),
     ],
     filter: Filters.All,
 }
 
 const initStore = () => {
-    console.log( state );
+    loadstore();
     console.log( 'Initstore' );
 }
 
 const loadstore = () => {
-    throw new Error('Not implemented');
+    if ( !localStorage.getItem('state') ) return;
+
+    const { todos = [], filter = Filters.All } = JSON.parse( localStorage.getItem('state') );
+    state.todos = todos;
+    state.filter = filter;
+}
+
+const saveStateToLocalStorage = () => {
+    localStorage.setItem( 'state', JSON.stringify( state ) );
 }
 
 const getTodos = ( filter = Filters.All ) => {
@@ -45,18 +55,33 @@ const getTodos = ( filter = Filters.All ) => {
 const addTodo = ( description ) => {
     if ( !description ) throw new Error('Descriotion is required');
     state.todos.push( new Todo( description ))
+
+    saveStateToLocalStorage();
 }
 
 const toggleTodo = ( todoId ) => {
+    state.todos = state.todos.map(todo => {
+        if ( todo.id === todoId) {
+            todo.done = !todo.done;
+        }
+        return todo;
+    })
+
+    saveStateToLocalStorage();
+
 }
 
 const deleteTodo = ( todoId ) => {
     state.todos = state.todos.filter( todo => todo.id !== todoId );
+    saveStateToLocalStorage();
+
     
 }
 
 const deleteCompleted = () => {
-    state.todos = state.todos.filter( todo => todo.done );
+    state.todos = state.todos.filter( todo => !todo.done );
+    saveStateToLocalStorage();
+
 }
 
 /**
@@ -64,9 +89,8 @@ const deleteCompleted = () => {
  * @param {Filters} newFilter 
  */
 const setFilter = ( newFilter = Filters.All ) => {
-    if ( Object.keys(Filters).includes( newFilter ) ) {
         state.filter = newFilter;
-    };
+        saveStateToLocalStorage();
 }
 
 const getCurrentFilter = () => {
